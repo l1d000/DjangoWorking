f827d97ac262d8a011bb48f1b5e4f73ab261a899
l1d000 <l1d000@hotmail.com>
2018-03-09 15:51:29 +0800

    register  rombuild

--- rombuild/admin.py
+++ rombuild/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from rombuild.models import BuildProject
+from django.forms import Textarea, TextInput
+from django.db import models
+
+class BuildProjectAdmin(admin.ModelAdmin):
+    list_display = ('Project_Name', 'build_path')
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'100'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':5, 'cols':100})},
+    }
+
+admin.site.site_header = 'Working'
+admin.site.site_title = 'Working' 
 
 # Register your models here.
+admin.site.register(BuildProject, BuildProjectAdmin)
--- rombuild/models.py
+++ rombuild/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
+class BuildProject(models.Model):
+    project_name   = models.CharField(max_length=100)
+    build_path   = models.CharField(max_length=100)
+    sync_command = models.CharField(max_length=100)
+    export_variables   = models.TextField()
+    build_command   = models.TextField()
+    color_code = models.CharField(max_length=6)
+    def Project_Name(self):
+        return format_html('<span style="color: #{0};">{1}</span>',
+                           self.color_code,self.project_name)
+    Project_Name.allow_tags = True
+
