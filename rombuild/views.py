9d4ec324e6a4a3170b2aa7001659cf3ea7841f12
l1d000 <l1d000@hotmail.com>
2018-03-14 19:11:45 +0800

    add input list

--- rombuild/views.py
+++ rombuild/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse  
 import json
 from  pytools.tool_shell import shell_command
+from models import BuildProject
 
 # Create your views here.
 def index(request):
     context          = {}
     context['title'] = 'Hello World!'
+    project_names = BuildProject.objects.all()
+    context['project_names'] = project_names
+    for i in project_names:
+        print(i.project_Name)
     return render(request, 'index.html', context)
 
 def animation(request):
