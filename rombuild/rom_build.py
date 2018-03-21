8c0d054680fba8950814be342c68e96a35a3d53c
l1d000 <l1d000@hotmail.com>
2018-03-21 15:47:34 +0800

    add build progerss

--- rombuild/rom_build.py
+++ rombuild/rom_build.py
@@ -2,15 +2,18 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 from django.http import HttpResponse
+from  pytools.tool_shell import shell_command, get_sync_current
+from models import BuildProject
 import threading
 import time
 import json
-from  pytools.tool_shell import shell_command, get_sync_current
-from models import BuildProject
+import re
+import os
 
 threadLock = threading.Lock()
 running = False
 current_name = ""
+current_path = ""
 
 class ShellThread (threading.Thread):
     def __init__(self, threadID, name, cmds):
@@ -25,7 +28,7 @@ class ShellThread (threading.Thread):
         threadLock.acquire()
         shell_run(self.name, self.cmds)
         threadLock.release()
-        running = False 
+        running = False
 
 def shell_run(threadName, cmds):
     shell_command(cmds)
@@ -39,6 +42,7 @@ def get_running_project():
 
 def rom_running(project_name):
     global current_name
+    global current_path
     try:
         project_info = BuildProject.objects.filter(project_Name=project_name)
         if project_info:
@@ -49,22 +53,41 @@ def rom_running(project_name):
             exe_cmd_list += project_info[0].sync_Command.replace("$ID", project_info[0].ssh_Name)\
                                                             .replace("$MIRROR", project_info[0].ssh_Mirror)
             exe_cmd_list += " ; repo sync -c;"
-            #exe_cmd_list += project_info[0].export_Variables
-            #exe_cmd_list += project_info[0].build_Command
+            exe_cmd_list += project_info[0].export_Variables
+            exe_cmd_list += project_info[0].build_Command
             print(exe_cmd_list)
             current_name = project_info[0].project_Name
+            current_path = project_info[0].build_Path+"/"+project_info[0].project_Name+"/out-log.txt"
+            print(current_path)
             shell_thread = ShellThread(1, "Thread-Shell-Running", exe_cmd_list)
-            shell_thread.start()  
+            shell_thread.start()
             return True
-    except BuildProject.DoesNotExist: 
+    except BuildProject.DoesNotExist:
             return False
     return False
-    
+
+def get_build_current(path, max_number):
+    current_percent = 0
+    with open(path, 'r') as f:
+            for line in f:
+                oneline = re.findall(r"[\d.]+%",line)
+                if oneline:
+                    string_line = re.findall(r"\[(.*?)\]",line)
+                    if string_line:
+                       string = re.findall(r"/(\d*)",string_line[0])
+                       newint = int(oneline[0].strip("%"))
+                       if int(string[0]) > max_number and newint> current_percent:
+                           current_percent = newint
+    return current_percent
+
 
 
 def get_sync_progress(request):
     num = {}
     num["progressbar_s"] = get_sync_current()
-    num["progressbar_b"] = 0
+    if os.path.exists(current_path):
+        num["progressbar_b"] = get_build_current(current_path, 100000)
+    else:
+        num["progressbar_b"] = 0
     print(json.dumps(num))
     return HttpResponse(json.dumps(num))
